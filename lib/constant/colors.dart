fd278e3c398cd2f816556e017b49043262791c27
Daeseong Kim <daeseongkim05@gmail.com>
2025-01-06 09:49:28 +0900

    chore: colors.dart 추가

---
+++ lib/constant/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF0B0B0B);
+  static const Color darkGray = Color(0xFF9E9E9E);
+  static const Color gray = Color(0xFFD9D9D9);
+  static const Color lightGray = Color(0xFFF7F7F7);
+  static const Color green = Color(0xFF0DBB85);
+  static const Color onError = Color(0xFFFD7563);
+}
