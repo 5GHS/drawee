f62905fb7a55697789930c057c4aaa28df01a9ea
marion419 <daynvi@gmail.com>
2025-01-06 10:40:44 +0900

    add: 마이페이지 제목 1 - 마음날씨별 보기

--- lib/presentation/ui/mypage/mypage_page.dart
+++ lib/presentation/ui/mypage/mypage_page.dart
@@ -5,9 +5,41 @@ class MypagePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Mypage Page'),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: ListView(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // 제목 1 - 마음 날씨별 보기
+                  const Text(
+                    '마음 날씨별 보기',
+                    style: TextStyle(
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20,
+                    ),
+                  ),
+                  // 페이지 이동 아이콘
+                  GestureDetector(
+                    onTap: () {
+                      print('tap');
+                    },
+                    child: Container(
+                      width: 100,
+                      alignment: Alignment.centerRight,
+                      color: Colors.transparent,
+                      child: const Icon(Icons.arrow_forward_ios),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
