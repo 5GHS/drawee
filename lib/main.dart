0d58df6981d495312c85e04fe40b1133d7eb3a6f
Daeseong Kim <daeseongkim05@gmail.com>
2025-01-07 02:10:56 +0900

    fix: 불필요한 코드와 설정 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,26 @@
+import 'package:drawee/add_mock_data.dart';
+import 'package:drawee/presentation/ui/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:drawee/presentation/pags/splash/splash_pags.dart';
 import 'package:drawee/firebase_options.dart';
 
 void main() async {
-  
-WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-runApp(const MyApp());
-}
-
-class DefaultFirebaseOptions {
-  static var currentPlatform;
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return const MaterialApp(
       title: 'drawee',
-      theme: ThemeData.dark(),
-      home: const SplashPage(),
+      home: AddMockData(),
     );
   }
 }
