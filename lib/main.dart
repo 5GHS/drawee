c2499b7717424474bbda0dcb6ba92afa8d318119
marion419 <daynvi@gmail.com>
2025-01-07 09:08:41 +0900

    delete: Firebase option settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
+import 'package:drawee/presentation/ui/search/search_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:drawee/presentation/pags/splash/splash_pags.dart';
 import 'package:drawee/firebase_options.dart';
 
 void main() async {
-  
-WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-runApp(const MyApp());
-}
-
-class DefaultFirebaseOptions {
-  static var currentPlatform;
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -26,7 +22,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'drawee',
       theme: ThemeData.dark(),
-      home: const SplashPage(),
+      //home: const SplashPage(),
+      home: SearchPage(),
     );
   }
 }
