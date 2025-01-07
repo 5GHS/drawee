e1336886213c772ec01e87804072d714ae1fee5d
Min-woos <wcn67888@gmail.com>
2025-01-07 14:22:22 +0900

    fix: ui 디자인 변경

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'drawee',
-      home: AddMockData(),
+      home: SplashPage(),
     );
   }
 }
