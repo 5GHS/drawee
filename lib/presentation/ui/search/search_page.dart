98317d609a2fac71350f7a9f64ce7bd7d5a4d0aa
marion419 <daynvi@gmail.com>
2025-01-07 09:08:17 +0900

    add: SearchPage

---
+++ lib/presentation/ui/search/search_page.dart
@@ -0,0 +1,21 @@
+import 'package:drawee/constant/colors.dart';
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            width: 362,
+            height: 40,
+            color: AppColors.gray,
+          )
+        ],
+      ),
+    );
+  }
+}
