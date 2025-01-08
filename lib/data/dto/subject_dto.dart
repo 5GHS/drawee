7e22cae858c16ca1df3c43bfd69456de5a5107c7
marion419 <daynvi@gmail.com>
2025-01-08 16:49:53 +0900

    add: subject_dto.dart 추가

---
+++ lib/data/dto/subject_dto.dart
@@ -0,0 +1,23 @@
+class SubjectDTO {
+  List<String> postsIds;
+  String topic;
+
+  SubjectDTO({
+    required this.postsIds,
+    required this.topic,
+  });
+
+  SubjectDTO.fromJson(Map<String, dynamic> json)
+      : this(
+          postsIds:
+              (json['postsIds'] as List).map((item) => item as String).toList(),
+          topic: json['topic'],
+        );
+
+  Map<String, dynamic> toJson() {
+    return {
+      'postsIds': postsIds,
+      'topic': topic,
+    };
+  }
+}
