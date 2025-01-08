4363632980df5836953759dc7db7539e7cb11f86
Daeseong Kim <daeseongkim05@gmail.com>
2025-01-08 18:09:19 +0900

    style: 전역 폰트 추가 및 appbar와 navigationBar 색상 고정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:drawee/add_mock_data.dart';
-import 'package:drawee/presentation/ui/splash/splash_page.dart';
-import 'package:drawee/presentation/ui/weather_post/weather_post_page.dart';
+import 'package:drawee/constant/colors.dart';
+import 'package:drawee/presentation/ui/layout/main_layout.dart';
 import 'package:drawee/core/utils/firestore_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -13,10 +12,11 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  await getFirestoreDocument(
-    collectionPath: 'posts',
-    documentId: '0MYZlzCIzmmyZvJfhmLi',
-  );
+  // * 데이터 추가할때 사용하세요
+  // await getFirestoreDocument(
+  //   collectionPath: 'posts',
+  //   documentId: '0MYZlzCIzmmyZvJfhmLi',
+  // );
 
   runApp(const ProviderScope(child: MyApp()));
 }
@@ -26,9 +26,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'drawee',
-      home: WeatherPostPage(),
+      theme: ThemeData(
+        fontFamily: 'Pretendard',
+        scaffoldBackgroundColor: AppColors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: AppColors.white,
+          foregroundColor: AppColors.black,
+          elevation: 0,
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: AppColors.white,
+          elevation: 0,
+        ),
+      ),
+      home: const MainLayout(),
     );
   }
 }
