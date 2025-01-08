ecb67326ed794297f856e6d7b0e9bb4f7e612a6d
Daeseong Kim <daeseongkim05@gmail.com>
2025-01-08 13:08:59 +0900

    feat: ProviderScope 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:drawee/add_mock_data.dart';
 import 'package:drawee/presentation/ui/splash/splash_page.dart';
+import 'package:drawee/presentation/ui/weather_post/weather_post_page.dart';
+import 'package:drawee/core/utils/firestore_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:drawee/firebase_options.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,7 +13,12 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MyApp());
+  await getFirestoreDocument(
+    collectionPath: 'posts',
+    documentId: '0MYZlzCIzmmyZvJfhmLi',
+  );
+
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -20,7 +28,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'drawee',
-      home: AddMockData(),
+      home: WeatherPostPage(),
     );
   }
 }
