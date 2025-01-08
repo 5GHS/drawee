71c754a2ce75eec998bc6bd33ff8509b038a9433
Min-woos <wcn67888@gmail.com>
2025-01-08 18:08:26 +0900

    fix : 불필요한 파일 삭제

--- lib/presentation/viewmodels/user_viewmodel.dart
+++
@@ -1,7 +0,0 @@
-import 'package:drawee/domain/usecases/fetch_user_data.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final fetchUserDataProvider = Provider((ref) {
-  final userRepository = ref.watch(userRepositoryProvider);
-  return FetchUserData(userRepository);
-});
