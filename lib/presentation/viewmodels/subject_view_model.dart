f2a9d692aa09985796f64fd6c24591c416a2f0a3
marion419 <daynvi@gmail.com>
2025-01-10 03:41:50 +0900

    add: SubjectViewmodel

---
+++ lib/presentation/viewmodels/subject_view_model.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+
+import 'package:drawee/domain/entities/subject.dart';
+import 'package:drawee/presentation/providers/subject_providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SubjectViewModel extends AsyncNotifier<List<Subject>> {
+  @override
+  FutureOr<List<Subject>> build() {
+    // 오늘의 주제 찾는 기능 완성하면
+    // 빌드에서 ref.read(getSubjectUsecaseProvider).excute() 리턴하도록 수정
+    return ref.read(getSubjectsUsecaseProvider).excute('');
+  }
+
+  Future<void> getSubjects(String query) async {
+    state = await AsyncValue.guard(
+        () => ref.read(getSubjectsUsecaseProvider).excute(query));
+  }
+}
