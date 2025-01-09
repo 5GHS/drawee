207f1ae74ad059e948d3fef47b1993a837e0de49
marion419 <daynvi@gmail.com>
2025-01-09 19:07:09 +0900

    feat: subjects 컬렉션을 algolia에 업로드하는 setSubjectsData 함수 생성

--- functions/index.js
+++ functions/index.js
@@ -9,17 +9,19 @@ const admin = require("firebase-admin");
 const algoliasearch = require("algoliasearch");
 const { initializeApp } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
+const { object } = require("firebase-functions/v1/storage");
+const { topic } = require("firebase-functions/v1/pubsub");
 
 // API KEY
 const ALGOLIA_APP_ID = "H464EGHSBZ";
 const ALGOLIA_API_KEY = "66cc719f55dc615b4e37378adfe04ff8";
 const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
-const index = client.initIndex("subjects");
 
 // recommend_subject 컬렉션을 algolia index에 업데이트
-async function setRecommendSubjectData() {
+async function setRecommendSubjectsData() {
   initializeApp();
 
+  const index = client.initIndex("recommend_subjects");
   const firestore = admin.firestore();
   const firestoreCollection = await firestore.collection("recommend_subjects");
 
@@ -28,6 +30,7 @@ async function setRecommendSubjectData() {
     if (!!snapshot) {
       console.log(snapshot.data());
       const record = {
+        objectID: "recommend",
         todayIdx: snapshot.data().todayIdx,
         subjectId: snapshot.data().subjectId,
       };
@@ -40,4 +43,42 @@ async function setRecommendSubjectData() {
   }
 }
 
-setRecommendSubjectData();
+// recommend_subject 컬렉션을 algolia index에 업데이트
+async function setSubjectsData() {
+  const recordCollection = [];
+
+  const index = client.initIndex("subjects");
+  const firestore = admin.firestore();
+  const firestoreCollection = await firestore.collection("subjects");
+
+  try {
+    const snapshot = await firestoreCollection.get();
+    if (!!snapshot) {
+      snapshot.forEach((element) => {
+        const record = {
+          topic: element.data().topic,
+        };
+        recordCollection.push(record);
+      });
+      await index
+        .saveObjects(recordCollection, { autoGenerateObjectIDIfNotExist: true })
+        .wait();
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// async function findSubjectInAlgolia(query) {
+//   const index = client.initIndex("subjects");
+//   try {
+//     const result = index.search(query);
+//     if (!!result) {
+//       return result;
+//     }
+//     return [];
+//   } catch (e) {
+//     console.log(e);
+//     return [];
+//   }
+// }
