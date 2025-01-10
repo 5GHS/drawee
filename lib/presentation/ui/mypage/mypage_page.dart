7cd8128fefbed15fcff594a2c82bcddc8f1b358a
Daeseong Kim <daeseongkim05@gmail.com>
2025-01-10 20:37:01 +0900

    feat: mypage 작성중...

--- lib/presentation/ui/mypage/mypage_page.dart
+++ lib/presentation/ui/mypage/mypage_page.dart
@@ -1,7 +1,9 @@
+import 'package:drawee/constant/colors.dart';
 import 'package:drawee/presentation/viewmodels/auth_view_model.dart';
 import 'package:drawee/presentation/viewmodels/user_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MypagePage extends ConsumerWidget {
   const MypagePage({super.key});
@@ -11,13 +13,40 @@ class MypagePage extends ConsumerWidget {
     final userState = ref.watch(userViewModelProvider);
 
     return Scaffold(
-      body: Center(
-        child: userState.when(
-          data: (userState) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
+      appBar: AppBar(
+        title: const Text(
+          '마이페이지',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        elevation: 0,
+      ),
+      body: userState.when(
+        data: (userState) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
               children: [
+                Container(
+                  height: 80,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: AppColors.lightGray)),
+                  child: ListTile(
+                    leading: const CircleAvatar(), // TODO: user Img 삽입 필요
+                    title: Text(
+                      userState.user?.name ?? "",
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w500, fontSize: 14),
+                    ),
+                    subtitle: Text(
+                      "쓴 글 ${userState.user?.likedPostsIds.length}회",
+                      style: const TextStyle(
+                          fontSize: 12, color: AppColors.darkGray),
+                    ),
+                    trailing: SvgPicture.asset('assets/icon/profile_edit.svg'),
+                  ),
+                ),
                 Text(userState.user?.id ?? ''),
                 Text(userState.user?.name ?? ''),
                 Text(userState.user?.imgUrl ?? ''),
@@ -38,11 +67,11 @@ class MypagePage extends ConsumerWidget {
                   child: const Text('회원탈퇴'),
                 ),
               ],
-            );
-          },
-          loading: () => const CircularProgressIndicator(),
-          error: (error, stack) => Text('Error: $error'),
-        ),
+            ),
+          );
+        },
+        loading: () => const CircularProgressIndicator(),
+        error: (error, stack) => Text('Error: $error'),
       ),
     );
   }
