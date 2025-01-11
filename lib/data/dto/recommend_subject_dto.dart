f53db3ecd744ea8e6277bae0bbe188ddcf97fe3e
marion419 <daynvi@gmail.com>
2025-01-11 18:25:59 +0900

    fix: RecommendSubjectDTO.fromJson 타입 factory로 변경

--- lib/data/dto/recommend_subject_dto.dart
+++ lib/data/dto/recommend_subject_dto.dart
@@ -1,23 +1,25 @@
 import 'package:drawee/domain/entities/recommend_subject.dart';
 import 'package:flutter/material.dart';
 
-class RecommendSubjectDto {
+class RecommendSubjectDTO {
   int subjectIndex;
   String subjectId;
   String topic;
 
-  RecommendSubjectDto({
+  RecommendSubjectDTO({
     required this.subjectIndex,
     required this.subjectId,
     required this.topic,
   });
 
-  RecommendSubjectDto.fromJson(Map<String, dynamic> json, int documentId)
-      : this(
-          subjectIndex: documentId,
-          subjectId: json['subjectId'],
-          topic: json['topic'],
-        );
+  factory RecommendSubjectDTO.fromJson(
+      Map<String, dynamic> json, int documentId) {
+    return RecommendSubjectDTO(
+      subjectIndex: documentId,
+      subjectId: json['subjectId'],
+      topic: json['topic'],
+    );
+  }
 
   RecommendSubject toEntity() {
     return RecommendSubject(
