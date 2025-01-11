464f47e62a54868cb27215c4002174f44f3d1a82
marion419 <daynvi@gmail.com>
2025-01-11 18:46:16 +0900

    add: GetRecommendSubjectUsecase 추가

---
+++ lib/domain/usecases/recommend_subject/get_recommend_subject_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:drawee/domain/entities/recommend_subject.dart';
+import 'package:drawee/domain/repositories/recommend_subject_repository.dart';
+
+class GetRecommendSubjectUsecase {
+  final RecommendSubjectRepository _repository;
+
+  GetRecommendSubjectUsecase(this._repository);
+
+  Future<RecommendSubject?> excute(int subjectIndex) {
+    return _repository.getRecommendSubject(subjectIndex);
+  }
+}
