068c6a9a4ea35e94ba15b06b6725416648532f11
marion419 <daynvi@gmail.com>
2025-01-11 17:49:17 +0900

    fix: functions-index.js에서 필요 없는 주석과 함수 삭제

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,5 @@
-// .env에서 변수를 불러올 수 없어 우선 임시로 실제 키를 입력했습니다.
-// 환경변수 사용하는 코드 :
-// const ALGOLIA_ID = process.env.ALGOLIA_APP_ID;
-// const ALGOLIA_ADMIN_KEY = process.env.ALGOLIA_ADMIN_KEY;
+// app id : algolia.app.id
+// api key : algolia.api.key
 
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
@@ -13,8 +11,8 @@ const { object } = require("firebase-functions/v1/storage");
 const { topic } = require("firebase-functions/v1/pubsub");
 
 // API KEY
-const ALGOLIA_APP_ID = "H464EGHSBZ";
-const ALGOLIA_API_KEY = "66cc719f55dc615b4e37378adfe04ff8";
+const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
+const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY;
 const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
 
 // recommend_subject 컬렉션을 algolia index에 업데이트
@@ -74,17 +72,3 @@ async function setSubjectsData() {
     console.log(e);
   }
 }
-
-// async function findSubjectInAlgolia(query) {
-//   const index = client.initIndex("subjects");
-//   try {
-//     const result = index.search(query);
-//     if (!!result) {
-//       return result;
-//     }
-//     return [];
-//   } catch (e) {
-//     console.log(e);
-//     return [];
-//   }
-// }
