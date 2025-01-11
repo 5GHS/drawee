c2d0ab88c06b60cc5ff987339677d6b1506b7a4b
marion419 <daynvi@gmail.com>
2025-01-11 17:47:15 +0900

    fix: RecommendSubject에 topic 필드 추가

--- lib/domain/entities/recommend_subject.dart
+++ lib/domain/entities/recommend_subject.dart
@@ -1,9 +1,11 @@
 class RecommendSubject {
   int subjectIndex;
   String subjectId;
+  String topic;
 
   RecommendSubject({
     required this.subjectIndex,
     required this.subjectId,
+    required this.topic,
   });
 }
