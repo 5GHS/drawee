f48210f564c5dad294bdcd7718f7bdf099bc5ee8
marion419 <daynvi@gmail.com>
2025-01-12 03:23:26 +0900

    feat: SubjectViewmodel에 추전 주제 검색하는 함수 추가

--- lib/presentation/viewmodels/subject_view_model.dart
+++ lib/presentation/viewmodels/subject_view_model.dart
@@ -1,18 +1,63 @@
 import 'dart:async';
-
 import 'package:drawee/domain/entities/subject.dart';
 import 'package:drawee/presentation/providers/subject_providers.dart';
+import 'package:drawee/presentation/viewmodels/recommend_subject_view_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SubjectViewModel extends AsyncNotifier<List<Subject>> {
   @override
   FutureOr<List<Subject>> build() async {
-    // 오늘의 주제 찾는 기능 완성하면
-    // 빌드에서 ref.read(getSubjectUsecaseProvider).excute() 리턴하도록 수정
-    return await ref.read(getSubjectsUsecaseProvider).excute('바');
+    final subjectToday = await getSubjectToday();
+    return subjectToday == null ? [] : [subjectToday];
   }
 
+  // 오늘의 주제를 Subject 객체로 반환
+  Future<Subject?> getSubjectToday() async {
+    state = const AsyncValue.loading();
+    try {
+      final recommendSubject = await ref
+          .read(recommendSubjectViewModelProvider.notifier)
+          .getTodayRecommendSubject();
+      if (recommendSubject == null) {
+        return null;
+      }
+      ref
+          .read(getSubjectUsecaseProvider)
+          .execute(recommendSubject.topic)
+          .then((value) {
+        return value;
+      });
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  // 오늘의 주제를 검색해서 상태값에 반영
+  Future<void> getListOfSubjectToday() async {
+    state = const AsyncValue.loading();
+    try {
+      final recommendSubject = await ref
+          .read(recommendSubjectViewModelProvider.notifier)
+          .getTodayRecommendSubject();
+      if (recommendSubject == null) {
+        return;
+      }
+      state = await AsyncValue.guard(() => ref
+              .read(getSubjectUsecaseProvider)
+              .execute(recommendSubject.topic)
+              .then((value) {
+            if (value == null) return [];
+            return [value];
+          }));
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // 검색어에 맞는 주제 리스트를 상태값에 반영
   Future<void> getSubjects(String query) async {
+    state = const AsyncValue.loading();
     state = await AsyncValue.guard(
         () => ref.read(getSubjectsUsecaseProvider).excute(query));
   }
