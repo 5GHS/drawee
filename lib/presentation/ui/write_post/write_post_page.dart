d621eed0c67c24100d7897be702d53e1b56d6a49
Yujin <yujin.mar.chung@gmail.com>
2025-01-12 11:17:18 +0900

    refactor: write post refactoring

--- lib/presentation/ui/write_post/write_post_page.dart
+++ lib/presentation/ui/write_post/write_post_page.dart
@@ -1,28 +1,53 @@
 import 'dart:io';
 import 'dart:math';
 import 'package:drawee/constant/colors.dart';
+import 'package:drawee/domain/usecases/post/create_post_usecase.dart';
+import 'package:drawee/presentation/providers/post_providers.dart';
+import 'package:drawee/presentation/ui/write_post/widgets/diary_input_field.dart';
 import 'package:drawee/presentation/ui/write_post/widgets/hint_text.dart';
 import 'package:drawee/presentation/ui/write_post/widgets/section_title.dart';
 import 'package:drawee/presentation/ui/write_post/widgets/weather_button.dart';
+import 'package:drawee/presentation/ui/write_post/write_post_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:image/image.dart' as img;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:pro_image_editor/pro_image_editor.dart';
 
-class WritePostPage extends StatefulWidget {
+import '../../../domain/entities/post.dart';
+
+class WritePostPage extends ConsumerStatefulWidget {
   const WritePostPage({super.key});
 
   @override
-  State<WritePostPage> createState() => _WritePageState();
+  ConsumerState<WritePostPage> createState() => _WritePageState();
 }
 
-class _WritePageState extends State<WritePostPage> {
-  File? _image; // 이미지 저장할 변수
-  Uint8List? _editedImage; // 편집된 이미지 저장할 변수
+enum Weather {
+  sunny,
+  rainy,
+  cloudy,
+  windy,
+  snowy,
+}
 
+class _WritePageState extends ConsumerState<WritePostPage> {
+  File? _image; // 이미지 저장할 변수
   final ImagePicker _picker = ImagePicker(); // 이미지 피커 인스턴스
+
+  final _subjectController = TextEditingController(); // 주제 입력 컨트롤러
+  final _titleController = TextEditingController(); // 제목 입력 컨트롤러
+  final _contentController = TextEditingController(); // 내용 입력 컨트롤러
+
+  Weather? _selectedWeather; // 선택된 날씨
+
+  @override
+  void dispose() {
+    _subjectController.dispose();
+    _titleController.dispose();
+    _contentController.dispose();
+    super.dispose();
+  }
 
   // 이미지 선택하고 ProImageEditor로 편집도 수행
   Future<void> _pickAndEditImage(ImageSource source) async {
@@ -83,9 +108,11 @@ class _WritePageState extends State<WritePostPage> {
         });
   }
 
-// /data/user/0/com.oghs.drawee/code_cache/line_art.png
   @override
   Widget build(BuildContext context) {
+    final state = ref.watch(writePostViewModelProvider); // 상태 읽기
+    final viewModel = ref.read(writePostViewModelProvider.notifier); // 메서드 호출
+
     return Scaffold(
         backgroundColor: AppColors.white,
         appBar: AppBar(title: Text('새 그림 일기')),
@@ -100,6 +127,7 @@ class _WritePageState extends State<WritePostPage> {
                 Container(
                   height: 40,
                   child: TextField(
+                    controller: _subjectController,
                     decoration: InputDecoration(
                       hintText: "글과 관련된 주제를 선택해 주세요",
                       hintStyle: TextStyle(
@@ -137,29 +165,23 @@ class _WritePageState extends State<WritePostPage> {
                 SizedBox(height: 16),
                 SectionTitle(text: '날씨'),
                 SizedBox(height: 16),
+                // 날씨 선택 버튼
                 Container(
                   height: 52,
                   child: Row(
                     children: [
-                      weatherButton(
-                        weather: 'sunny',
-                      ),
-                      SizedBox(width: 14),
-                      weatherButton(
-                        weather: 'rainy',
-                      ),
-                      SizedBox(width: 14),
-                      weatherButton(
-                        weather: 'cloudy',
-                      ),
-                      SizedBox(width: 14),
-                      weatherButton(
-                        weather: 'windy',
-                      ),
-                      SizedBox(width: 14),
-                      weatherButton(
-                        weather: 'snowy',
-                      ),
+                      for (final weather in Weather.values) ...[
+                        if (weather != Weather.sunny) const SizedBox(width: 14),
+                        weatherButton(
+                          weather: weather.name,
+                          isSelected: _selectedWeather == weather,
+                          onTap: () {
+                            setState(() {
+                              _selectedWeather = weather;
+                            });
+                          },
+                        ),
+                      ],
                     ],
                   ),
                 ),
@@ -207,36 +229,21 @@ class _WritePageState extends State<WritePostPage> {
                   ),
                 ),
                 SizedBox(height: 16),
+                // 일기 제목
                 Container(child: SectionTitle(text: '일기 제목')),
                 SizedBox(height: 16),
-                Container(
-                  height: 42,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      contentPadding:
-                          EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                      hintText: "일기 제목을 적어주세요",
-                      hintStyle: hintTextStyle,
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: AppColors.gray),
-                          borderRadius: BorderRadius.circular(20)),
-                    ),
-                  ),
-                ),
+                DiaryInputField(
+                    titleController: _titleController,
+                    height: 42,
+                    hintText: "일기 제목을 적어주세요"),
                 SizedBox(height: 16),
+                // 일기 내용
                 SectionTitle(text: '일기 내용'),
                 SizedBox(height: 16),
-                TextField(
-                  decoration: InputDecoration(
-                      contentPadding:
-                          EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                      hintText: "일기 내용을 적어주세요",
-                      hintStyle: hintTextStyle,
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: AppColors.gray),
-                          borderRadius: BorderRadius.circular(20))),
-                  maxLines: 10,
-                ),
+                DiaryInputField(
+                    titleController: _contentController,
+                    height: 167,
+                    hintText: "일기 내용을 적어주세요"),
                 SizedBox(height: 30),
 
                 // 일기 업로드 버튼
@@ -245,7 +252,9 @@ class _WritePageState extends State<WritePostPage> {
                   height: 54,
                   width: double.infinity,
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      // TODO: Implement upload functionality
+                    },
                     child: Text(
                       "일기 업로드",
                       style: TextStyle(
